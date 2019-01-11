ea6430a998bbd9bceae33a9b6a8b88fcfacb5223
Prendem <46231321+Prendem@users.noreply.github.com>
2019-01-11 00:38:39 +0000

    Create deck.h

---
+++ deck.h
@@ -0,0 +1,54 @@
+#ifndef DECK_H
+#define DECK_H
+#include <vector>
+
+class Hand;
+
+enum class Suit
+{
+	CLUBS,
+	SPADES,
+	HEARTS,
+	DIAMONDS,
+	MAX_SUITS
+};
+
+enum class Rank
+{
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN,
+	JACK,
+	QUEEN,
+	KING,
+	ACE,
+	MAX_RANKS
+};
+
+struct Card
+{
+	Suit suit;
+	Rank rank;
+};
+
+class Deck
+{
+private:
+	
+	int m_topcard;
+public:
+	std::vector<Card> m_deck;
+	Deck();
+	Card deal();
+	int randomNumber(int min, int max);
+	void reset();
+	void shuffle();
+};
+
+#endif
